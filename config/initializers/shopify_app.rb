1fb524c9585009174cf723ebab17cfd445573d77
Michael Hewson <michael@michaelhewson.ca>
2017-08-03 15:22:23 -0400

    rename app

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
+  config.application_name = "Polaris Rails Demo"
   config.api_key = ENV.fetch 'SHOPIFY_API_KEY'
   config.secret = ENV.fetch 'SHOPIFY_SECRET'
   config.scope = "read_orders, read_products"
