8943bf6277b686cf08bfdccd12ed6b58168c8215
Michael Hewson <michael@michaelhewson.ca>
2017-08-10 18:25:11 -0400

    fix redirect

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: redirect('/login')
+  root to: redirect('embedded', status: 302)
 
   get 'embedded', to: 'embedded_app#index'
   get 'embedded/*other', to: 'embedded_app#index'
